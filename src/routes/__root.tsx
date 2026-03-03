a3db08596dbf280f15e345796460d0abe02796ab
oliver-gc <olivergchester@gmail.com>
2026-03-03 23:13:16 +0000

    Fixing hydration css

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -24,7 +24,6 @@ const themeScript = `(function () {
     var root = document.documentElement;
     root.classList.remove('light', 'dark');
     root.classList.add(resolved);
-    root.classList.add('theme-ready');
   } catch (e) {}
 })();`
 
