fb563e965898461dbd9cdb4f22637e3fbbe8c0b7
oliver-gc <olivergchester@gmail.com>
2026-03-04 19:24:05 +0000

    Fixing nitro config

--- nitro.config.ts
+++ nitro.config.ts
@@ -9,16 +9,19 @@ export default defineConfig({
     "/assets/**": {
       headers: {
         "cache-control": "public, max-age=31536000, immutable",
+        "cdn-cache-control": "public, max-age=31536000, immutable",
       },
     },
     "/api/**": {
       headers: {
         "cache-control": "no-store",
+        "cdn-cache-control": "no-store",
       },
     },
     "/**": {
       headers: {
         "cache-control": "no-store",
+        "cdn-cache-control": "no-store",
       },
     },
   },
